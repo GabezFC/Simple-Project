433d71eeeccd7845e7863676758452454cabf6ce
Gabriel <fonsecacabralg@gmail.com>
2026-07-30 17:06:29 -0400

    Adicionando Nunpy ao projeto
    Refactor movement logic to use NumPy array for the world grid and improve position updating.

--- Movimento.py
+++ Movimento.py
@@ -1,27 +1,32 @@
-mundo = [
+import numpy as np
+mundo = np.array([
     ["*", "*", "*"],
     ["*", "*", "*"],
     ["*", "*", "*"]
-]
-
+])
 linha = 0
 coluna = 0
-
 mundo[linha][coluna] = "#"
-
+print (mundo)
 def movimento(loc):
     global linha
-
-    try:
-        if loc == "s":
-            mundo[linha][coluna] = "*"   # apaga posição antiga
-            linha += 1
-            mundo[linha][coluna] = "#"   # desenha na nova posição
-
-    except Exception as error:
-        print(f"Houve um erro: {error}")
-
+    global coluna
+    global mundo
+    
+    jogando = True
+    while jogando:
+        try:
+            if loc == "s":
+                linha += 1
+                mundo[linha][coluna] = "#"   # desenha na nova posição
+                print(mundo)
+            
+    
+        except Exception as error:
+            print(f"Houve um erro: {error}")
     print(mundo)
-
 presskey = input()
 movimento(presskey)
+
+
+Por que está dando erro
